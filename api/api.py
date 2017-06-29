1d9bce55e6cdfd0faa2161ff03763beaab1b13a0
Roger Taracha <rojtaracha@gmail.com>
2017-06-29 16:57:34 +0300

    Working with resourceful routing on top of Flask pluggable views:   + Create a MessageList class to represent the collection of messages.

--- api/api.py
+++ api/api.py
@@ -70,11 +70,16 @@ class Message(Resource):
                 status.HTTP_404_NOT_FOUND,
                 message = "Message {0} doesn't exist".format(id))
 
+# @marshal_with decorator takes message_fields as an argument.
+# The decorator takes MessageModel instance and applies the field filtering
+# and output formatting specified in message_fields.
+# NB: Using @marshal_with decorator, automatically returns a HTTP 200 OK status code.
     @marshal_with(message_fields)
     def get(self, id):
         self.abort_if_message_doesnt_exist(id)
         return message_manager.get_message(id)
 
+    @marshal_with(message_fields)
     def delete(self, id):
         self.abort_if_message_doesnt_exist(id)
         message_manager.delete_message(id)
@@ -83,12 +88,45 @@ class Message(Resource):
     @marshal_with(message_fields)
     def patch(self, id):
         self.abort_if_message_doesnt_exist(id)
-        message =message_manager.get_message(id)
+        message = message_manager.get_message(id)
         parser = reqparse.RequestParser()
         parser.add_argument('message', type=str)
         parser.add_argument('duration', type=int)
         parser.add_argument('printed_times', type=int)
         parser.add_argument('printed_once', type=bool)
+
+        # Call the parser.parse_args method to parse all the arguments
+        # from the request and save the returned dictionary in the args variable.
         args = parser.parse_args()
+
         if 'message' in args:
-            
+            message.message = args['message']
+        if 'duration' in args:
+            message.duration = args['duration']
+        if 'printed_times' in args:
+            message.printed_times = args['printed_times']
+        if 'printed_once' in args:
+            message.printed_once = args['printed_once']
+        return message
+
+# MessageList class that represents the collection of messages.
+class MessageList(Resource):
+    @marshal_with(message_fields)
+    def get(self):
+        return [v for v in message_manager.messages.values()]
+
+    @marshal_with(message_fields)
+    def post(self):
+        parser = request.RequestParser()
+        parser.add_argument('message', type=str, required=True, help='Message cannot be blank!')
+        parser.add_argument('duration', type=int, required=True, help='Duration cannot be blank!')
+        parser.add_argument('message_category', type=str, required=True, help='Message category cannot be blank!')
+        args = parser.parse_args()
+        message = MessageModel(
+            message=args['message'],
+            duration=args['duration'],
+            creation_date=datetime.now(utc),
+            message_category=args['message_category']
+        )
+        message_manager.insert_message(message)
+        return message, status.HTTP_201_CREATED
