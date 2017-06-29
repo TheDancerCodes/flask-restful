9f2b49b52777ad28c39a186486110fff857104bf
Roger Taracha <rojtaracha@gmail.com>
2017-06-29 15:46:09 +0300

    Working with resourceful routing on top of Flask pluggable views:   + Create a Message class to represent the message resource.

--- api/api.py
+++ api/api.py
@@ -59,3 +59,36 @@ message_fields = {
 # create an instance of the previously created MessageManager class.
 # We use this instance to create, retrieve, and delete MessageModel instances.
 message_manager = MessageManager()
+
+
+# This is a subclass of flask_restful.Resource and represents a RESTful Resource
+# We declare methods for each supported HTTP verb here.
+class Message(Resource):
+    def abort_if_message_doesnt_exist(self, id):
+        if id not in message_manager.messages:
+            abort(
+                status.HTTP_404_NOT_FOUND,
+                message = "Message {0} doesn't exist".format(id))
+
+    @marshal_with(message_fields)
+    def get(self, id):
+        self.abort_if_message_doesnt_exist(id)
+        return message_manager.get_message(id)
+
+    def delete(self, id):
+        self.abort_if_message_doesnt_exist(id)
+        message_manager.delete_message(id)
+        return '', status.HTTP_204_NO_CONTENT
+
+    @marshal_with(message_fields)
+    def patch(self, id):
+        self.abort_if_message_doesnt_exist(id)
+        message =message_manager.get_message(id)
+        parser = reqparse.RequestParser()
+        parser.add_argument('message', type=str)
+        parser.add_argument('duration', type=int)
+        parser.add_argument('printed_times', type=int)
+        parser.add_argument('printed_once', type=bool)
+        args = parser.parse_args()
+        if 'message' in args:
+            
