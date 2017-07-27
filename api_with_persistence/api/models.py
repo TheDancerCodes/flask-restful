a63c599637562948d720cb5e820663f8b8062827
Roger Taracha <rojtaracha@gmail.com>
2017-07-27 13:24:12 +0300

    Creating schemas to Validate, Serialize, and Deserialize models: [+] Added CategorySchema, MessageSchema & process_category method. [+] These Flask-Marshmallow schemas are  use to validate, serialize, and deserialize the previously declared Category and Message models and their relationships.

--- api_with_persistence/api/models.py
+++ api_with_persistence/api/models.py
@@ -30,7 +30,7 @@ class AddUpdateDelete():
         db.session.add(resource)
         return db.session.commit()
 
-# db.relationship function provides a many-to-one relationship to the Category model. 
+# db.relationship function provides a many-to-one relationship to the Category model.
 class Message(db.Model, AddUpdateDelete):
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String(250), unique=True, nullable=False)
@@ -53,3 +53,47 @@ class Category(db.Model, AddUpdateDelete):
 
     def __init__(self, name):
         self.name = name
+
+
+# Flask-Marshmallow schemas to validate, serialize, and deserialize the
+# previously declared Category and Message models and their relationships.
+
+# We have two objects that nest to each other, that is,
+# We will create a two-way nesting between categories and messages.
+class CategorySchema(ma.Schema):
+    id = fields.Integer(dump_only=True)
+    name = fields.String(required=True, validate=validate.Length(3))
+    url = ma.URLFor('api.categoryresource', id='<id>', _external=True)
+    messages = fields.Nested('MessageSchema', many=True, exclude=('category',))0029
+
+class MessageSchema(ma.Schema):
+    id = fields.Integer(dump_only=True)
+    message = fields.String(required=True, validate=validate.Length(1))
+    duration = fields.Integer()
+    creation_date = fields.DateTime()
+    category = fields.Nested(CategorySchema, only=['id', 'url', 'name'], required=True)
+    printed_times = fields.Integer()
+    printed_once = fields.Boolean()
+    url = ma.URLFor('api.messageresource', id='<id>', _external=True)
+
+
+ # This decorator registers a method to invoke before deserializing an object.
+ # Before Marshmallow deserializes a message, the process_category method will be executed.
+
+ # The code in the process_category method checks the value for the 'category'
+ # key and returns a dictionary with the appropriate data to make it sure that
+ # we are able to deserialize a category with the appropriate key-value pairs,
+ # no matter the differences of the incoming data.
+    @pre_load
+    def process_category(self, data):
+        category = data.get('category')
+        if category:
+            if isinstance(category, dict):
+                category_name = category.get('name')
+            else:
+                category_name = category
+            category_dict = dict(name=category_name)
+        else:
+            category_dict = {}
+        data['category'] = category_dict
+        return data
